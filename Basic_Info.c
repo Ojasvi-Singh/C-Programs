5ac644f29fdd4edbd0d4534f913cbb6d87c6c15d
Ojasvi-Singh <84854325+Ojasvi-Singh@users.noreply.github.com>
2025-08-14 08:26:44 +0530

    Add files via upload

---
+++ Basic_Info.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    printf("Name:Ojasvi Singh");
+    printf("\nDOB:17/05/2007");
+    printf("\nAge:18");
+    printf("\nSAP ID:590025857");
+    
+    return 0;
+}
---
+++ faranhite_to_celsius.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float celcius,faran;
+    printf("Enter temperature in faranhite: ")
+    scanf("%f", &faran);
+    celcius=(faren-32)*5/9;
+    printf("Temperature in celcius is ");
+    printf("%f",celcius);
+    return 0;
+}
